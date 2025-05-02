e4496523db19d2ae207f3e614f7834e3ab8a12e2
Richard Tello <145090121+rgodinezt@users.noreply.github.com>
2025-05-02 10:05:39 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-function setResource(file, elementSelector) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.querySelector(elementSelector).innerHTML = data;
-        });
-}
